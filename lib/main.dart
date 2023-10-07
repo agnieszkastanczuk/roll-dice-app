dbe946631cd8683b8ac0f6b33336d9df432b464d
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-10-08 00:31:12 +0200

    feat: add custom class

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello world!'),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.pink,
+            Colors.white24,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
